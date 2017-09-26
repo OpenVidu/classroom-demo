f13c0b575dbb20734da6440c36d11d4f830642eb
pabloFuente <pablofuenteperez@gmail.com>
2017-09-26 18:15:32 +0200

    classroom-demo container built over compiled openvidu-server

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,3 +1,6 @@
+# Copy compiled openvidu-server.jar
+cp ../../openvidu/openvidu-server/target/openvidu-server-"$1".jar ./openvidu-server.jar
+
 ### classroom-demo ###
 
 cd ../src/angular/frontend
@@ -10,22 +13,7 @@ mvn clean compile package -DskipTests=true
 
 cp target/classroom-demo-"$1".war docker/classroom-demo.war
 
-
-
-### openvidu-server ###
-
-# Copy openvidu-server project in docker build path except angular-cli project ('frontend' folder)
 cd docker
-rsync -ax --exclude='**/angular' ../../openvidu/openvidu-server .
-
-# Build and package maven project
-cd openvidu-server && mvn clean compile package -DskipTests=true
-
-# Copy openvidu.server.jar in docker build path
-cp target/openvidu-server-"$1".jar ../openvidu-server.jar
-
-
-cd ..
 
 docker build -t openvidu/classroom-demo .
 
--- src/main/resources/static/main.bundle.js
+++ src/main/resources/static/main.bundle.js
@@ -5502,13 +5502,10 @@ var User = (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
 var environment = {
     production: false,
-    URL_BACK: 'https://localhost:5000'
+    envName: 'container',
+    URL_BACK: window.location.origin
 };
 //# sourceMappingURL=environment.js.map
 
