62e46077941422c7e2ac74029d7353c9625e198f
pabloFuente <pablofuenteperez@gmail.com>
2018-06-28 13:20:02 +0200

    Update to 2.2.0

--- src/angular/BuildFrontWarAndRun.sh
+++ src/angular/BuildFrontWarAndRun.sh
@@ -3,4 +3,4 @@ cd frontend
 ng build --output-path ../../main/resources/static
 cd ../../../
 mvn clean package
-java -jar target/classroom-demo-2.0.0.war
+java -jar target/classroom-demo-2.2.0.war
--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1864,10 +1864,13 @@ var VideoSessionService = /** @class */ (function () {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
 var environment = {
     production: false,
-    envName: 'container',
-    URL_BACK: window.location.origin
+    URL_BACK: 'https://localhost:5000'
 };
 
 
