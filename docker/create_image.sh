a00f3b9c3ef808261d5bc43d40afcdff392942cd
cruizba <charlesrb94@outlook.com>
2021-06-02 20:53:33 +0200

    Make Dockerfile agnostic of the openvidu-server version

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -3,4 +3,4 @@ pushd ../
 export SOFTWARE_VERSION=$(grep -oPm1 "(?<=<version>)[^<]+" "pom.xml")
 
 docker build -f docker/Dockerfile -t openvidu/openvidu-classroom-demo .
-docker tag openvidu/openvidu-classroom:latest openvidu/openvidu-classroom-demo:$SOFTWARE_VERSION
+docker tag openvidu/openvidu-classroom-demo:latest openvidu/openvidu-classroom-demo:$SOFTWARE_VERSION
