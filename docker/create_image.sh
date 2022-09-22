fc9a7838097d857e8b17305e8ae3c7dec5217d7f
cruizba <carlos.ruizbal@gmail.com>
2022-09-22 14:37:39 +0200

    create_image.sh receives name of image as argument

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
-    echo "No version argument provided. Usage: \"./create_image.sh X.Y.Z\""
+    echo "No version argument provided. Usage: \"./create_image.sh <IMAGE_NAME>\""
     exit 1
 fi
 
 pushd ../
 
-docker build -f docker/Dockerfile -t openvidu/openvidu-classroom-demo:"${1}" .
+docker build -f docker/Dockerfile -t "$1" .
